f33cd23f798dea4eeeb3d05761b920efc9291655
GeneralDido <dimpanouris@gmail.com>
2016-03-03 02:21:16 +0100

    fix js form

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+ // Closes the sidebar menu
+$("#menu-close").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Opens the sidebar menu
+$("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#sidebar-wrapper").toggleClass("active");
+});
+
+// Scrolls to the selected menu item on the page
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top - 70
+                }, 600);
+                return false;
+            }
+        }
+    });
+});
+ var message = "";
+
+ $("form").on("submit", function() {
+     message = $("#contactform").serialize();
+     $.ajax({
+         url: "//formspree.io/dimpanouris@gmail.com",
+         method: "POST",
+         data: {message: message},
+         dataType: "json"
+     });
+     alert('Thank you very much! We\'ll contact you soon!');
+     var contactform =  document.getElementById('contactform');
+     contactform.reset();
+     return false;
+ });
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-74623303-2', 'auto');
+ga('send', 'pageview');
